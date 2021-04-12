0705d984a0bddd7017930a7de59002f22c230daa
Saurabh Mittal <saurabhmittal1606@gmail.com>
2021-04-12 19:53:41 +0530

    Add script

---
+++ script.py
@@ -0,0 +1,78 @@
+import cv2
+from scenedetect import VideoManager
+from scenedetect import SceneManager
+
+# For content-aware scene detection:
+from scenedetect.detectors import ContentDetector
+
+
+def variance_of_laplacian(image):
+    # compute the Laplacian of the image and then return the focus
+    # measure, which is simply the variance of the Laplacian
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
+
+def find_scenes(video_path, threshold=30.0):
+    # Create our video & scene managers, then add the detector.
+    video_manager = VideoManager([video_path])
+    scene_manager = SceneManager()
+    scene_manager.add_detector(
+        ContentDetector(threshold=threshold))
+
+    # Improve processing speed by downscaling before processing.
+    video_manager.set_downscale_factor()
+
+    # Start the video manager and perform the scene detection.
+    video_manager.start()
+    scene_manager.detect_scenes(frame_source=video_manager)
+
+    # Each returned scene is a tuple of the (start, end) timecode.
+    return scene_manager.get_scene_list()
+
+
+INPUT_PATH = './videos/tourism.mp4'
+
+scenes = find_scenes(INPUT_PATH)
+scene_ranges = []
+blurr_score = []
+
+# Find start and end frame of each scene
+for (start, end) in scenes:
+    scene_ranges.append((start.get_frames(), end.get_frames()))
+
+vidcap = cv2.VideoCapture(INPUT_PATH)
+success, image = vidcap.read()
+count = 0
+
+# Measure non-blurry score of each frame
+while success:
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    fm = variance_of_laplacian(gray)
+
+    blurr_score.append(fm)
+    success, image = vidcap.read()
+    count += 1
+
+best_frames = []
+
+# Find frame with max score in each scene range
+for i, (start, end) in enumerate(scene_ranges):
+    scene = blurr_score[start:end]
+    best_score = max(scene)
+    best_frame = scene.index(best_score)
+    best_frames.append(start + best_frame)
+
+vidcap = cv2.VideoCapture(INPUT_PATH)
+success, image = vidcap.read()
+count = 0
+ind = 0
+
+# Save best scored frames for each scene
+while success and ind < len(best_frames):
+    if (count == best_frames[ind]):
+        cv2.imwrite('scenes/tourism_scene{}.jpg'.format(ind), image)
+        ind += 1
+
+    blurr_score.append(fm)
+    success, image = vidcap.read()
+    count += 1
